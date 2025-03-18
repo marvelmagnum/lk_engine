836d46b3e4876061247a796dc7bb4175bdb316f9
Sourav Tosh <marvel_magnum@yahoo.com>
2025-03-18 02:16:16 -0300

    updated game data. improved reference detection

--- bookdata_builder.py
+++ bookdata_builder.py
@@ -24,7 +24,7 @@ def parse_section(section_text):
             text_content = text_content.replace("<i>Healt</i>h", "<i>Health</i>")
     
     # Find all "Turn to/ turn to <number>" references
-    references = re.findall(r'[Tt]urn to[ ]?<b>[ ]?(\d+)[., ]*</b>', ref_text)
+    references = re.findall(r'[Tt]urn (?:back )?to[ ]?<b>[ ]?(\d+)[., ]*</b>', ref_text)
     
     # Adjust the references by subtracting the offset
     references = [str(int(ref)) for ref in references]
